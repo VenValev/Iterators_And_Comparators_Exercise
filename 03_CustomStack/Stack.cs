6fa0989c38a6d0ff3cc7b933f7a36c5c1e743644
Vencislav <vencislav67@hotmail.com>
2022-07-12 11:53:57 +0300

    3

---
+++ 03_CustomStack/Stack.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03_CustomStack
+{
+    public class Stack
+    {
+
+    }
+}
---
+++ 03_CustomStack/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03_CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command;
+
+            while((command = Console.ReadLine()) != "END")
+            {
+
+            }
+        }
+    }
+}
