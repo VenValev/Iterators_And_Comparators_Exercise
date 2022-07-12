3f809ff3955abd47629832d8d115133ae1f0d4b9
Vencislav <vencislav67@hotmail.com>
2022-07-12 12:53:34 +0300

    4

---
+++ 04_Froggy/Prog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04_Froggy
+{
+    public class Prog
+    {
+        static void Main(string[] args)
+        {
+            var lake = new Lake(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
