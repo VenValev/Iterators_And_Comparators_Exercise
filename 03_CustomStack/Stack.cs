ae1e6f1acbade16c418b229ac710860921d44715
Vencislav <vencislav67@hotmail.com>
2022-07-12 11:58:05 +0300

    3

--- 03_CustomStack/Stack.cs
+++ 03_CustomStack/Stack.cs
@@ -1,11 +1,35 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace _03_CustomStack
 {
-    public class Stack
+    public class Stack<T> : IEnumerable<T>
     {
 
+        private List<T> list;
+
+        public Stack(params T[] data)
+        {
+            list = new List<T>();
+        }
+
+        public void Push(params T[] elements)
+        {
+            foreach(T element in elements)
+            {
+                list.Insert(list.Count, element);
+            }
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
