a52f2fc150e3204fbc6ed11c506153962ba83655
Vencislav <vencislav67@hotmail.com>
2022-07-13 11:58:54 +0300

    5

--- 05_Comparing_Objects/Program.cs
+++ 05_Comparing_Objects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05_Comparing_Objects
 {
@@ -6,7 +7,29 @@ namespace _05_Comparing_Objects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Person> persons = new List<Person>();
+            string command;
+
+            while((command = Console.ReadLine()) != "END")
+            {
+                var cmndArg = command.Split();
+
+                string personName = cmndArg[0];
+                int personAge = int.Parse(cmndArg[1]);
+                string personTown = cmndArg[2];
+
+                persons.Add(new Person(personName, personAge, personTown));
+
+            }
+
+            var index = int.Parse(Console.ReadLine());
+            var equal = 0;
+            var notEqual = 0;
+
+            foreach(Person person in persons)
+            {
+
+            }
         }
     }
 }
